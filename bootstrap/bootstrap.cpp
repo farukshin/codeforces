888e6d8576410cc9b9a3bfb38db851872c0f027a
Amir Farukshin <farukshin.amir@gmail.com>
2020-06-22 22:06:33 +0300

    docs(): добавил bootstrap для решений

---
+++ bootstrap/bootstrap.cpp
@@ -0,0 +1,27 @@
+// https://codeforces.com/contest/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ios_b                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    ;
+}
+
+int main()
+{
+    ios_b;
+#ifdef _DEBUG
+    freopen("input-1.txt", "r", stdin);
+    //freopen("output-1.txt", "w", stdout);
+#endif
+    solve();
+    return 0;
+}
