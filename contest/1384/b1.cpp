27a00d8f6c555c25643a389774132fe209567847
Amir Farukshin <farukshin.amir@gmail.com>
2020-07-25 21:20:31 +0300

    docs(): зафиксировал решения задач A, B1, B2 контеста 1384

---
+++ contest/1384/b1.cpp
@@ -0,0 +1,63 @@
+// https://codeforces.com/contest/1384/problem/B1
+// #tech_debt
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    ll n, k, l;
+    cin >> n >> k >> l;
+
+    ll x = 0, pz = k, z;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        ll d;
+        cin >> d;
+        z = l - d;
+
+        if (z < 0)
+        {
+            cout << "No" << endl;
+            return;
+        }
+        if (z >= k)
+            x = 0;
+        else if (x < k)
+            x = max(x + 1, k - z);
+        else if (x >= k)
+        {
+            x++;
+            if (x - k > z)
+            {
+                cout << "No" << endl;
+                return;
+            }
+        }
+
+        pz = max(z, k);
+    }
+    cout << "Yes" << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("b1-input-1.txt", "r", stdin);
+    //freopen("b1-output-1.txt", "w", stdout);
+#endif
+
+    //solve();
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
