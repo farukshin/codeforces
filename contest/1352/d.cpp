4d1059cf6a65e9f8eb9371964a87dbe1477f4060
Amir Farukshin <farukshin.amir@gmail.com>
2020-10-01 19:30:00 +0300

    docs(): 1352 D

---
+++ contest/1352/d.cpp
@@ -0,0 +1,26 @@
+// https://codeforces.com/contest/1352/problem/D
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    ;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("d-input-1.txt", "r", stdin);
+    //freopen("d-output-1.txt", "w", stdout);
+#endif
+
+    solve();
+    //int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
