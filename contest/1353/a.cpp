6353635495334e71431937e56c6be53450834a3e
Amir Farukshin <farukshin.amir@gmail.com>
2020-10-02 19:30:00 +0300

    docs(): 1353 A

---
+++ contest/1353/a.cpp
@@ -0,0 +1,28 @@
+// https://codeforces.com/contest/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    cout << min(n - 1, 2) * m << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+#ifdef _DEBUG
+    freopen("a-input-1.txt", "r", stdin);
+    //freopen("a-output-1.txt", "w", stdout);
+#endif
+
+    //solve();
+    int t; cin >> t; while(t--) solve();
+
+    return 0;
+}
